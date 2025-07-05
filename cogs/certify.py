d5086a1bc45e86d8a6bea1e4969937391e9b42b9
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-07-05 16:42:31 +0100

    Update certify.py

--- cogs/certify.py
+++ cogs/certify.py
@@ -72,7 +72,7 @@ class Certify(commands.Cog):
         officer_font_path = os.path.join(os.path.dirname(__file__), "AlegreyaSC-Regular.ttf")
 
         # Font sizes
-        cert_font_size = 76
+        cert_font_size = 72
         person_font_size = 46   # Change as desired
         officer_font_size = 46  # Change as desired
 
@@ -109,8 +109,8 @@ class Certify(commands.Cog):
         draw_spaced_text(draw, (cert_start_x, y_cert), certificate_name, cert_font, "black", cert_spacing)
 
         # The other fields use fixed positions
-        draw_spaced_text(draw, (575, 1380), person_name, person_font, "black", person_spacing)
-        draw_spaced_text(draw, (420, 1448), officer_name, officer_font, "black", officer_spacing)
+        draw_spaced_text(draw, (575, 1382), person_name, person_font, "black", person_spacing)
+        draw_spaced_text(draw, (420, 1450), officer_name, officer_font, "black", officer_spacing)
 
         # Save to buffer
         output_buffer = BytesIO()
