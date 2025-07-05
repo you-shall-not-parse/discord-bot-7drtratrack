3617beb173fd454708821bcc14b35dd5af8732bc
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-07-05 16:17:45 +0100

    Update certify.py

--- cogs/certify.py
+++ cogs/certify.py
@@ -69,9 +69,9 @@ class Certify(commands.Cog):
         # Adjust positions and spacing as needed
         spacing = 5  # Adjust this value to increase/decrease character spacing
 
-        draw_spaced_text(draw, (365, 920), certificate_name, font, "black", spacing=15)
-        draw_spaced_text(draw, (575, 1340), person_name, font, "black", spacing=15)
-        draw_spaced_text(draw, (420, 1400), officer_name, font, "black", spacing=15)
+        draw_spaced_text(draw, (400, 1200), certificate_name, font, "black", spacing=15)
+        draw_spaced_text(draw, (575, 1360), person_name, font, "black", spacing=15)
+        draw_spaced_text(draw, (420, 1430), officer_name, font, "black", spacing=15)
 
         # Save to buffer
         output_buffer = BytesIO()
