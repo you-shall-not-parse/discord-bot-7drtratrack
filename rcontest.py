6a6eb5ea2221f218a934ca18ceb8391953feb2bc
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-07-13 16:59:40 +0100

    Update rcontest.py

--- rcontest.py
+++ rcontest.py
@@ -1,7 +1,7 @@
 import socket
 
 SERVER_IP = '176.57.171.44'
-SERVER_PORT = 28025
+SERVER_PORT = 28015
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((SERVER_IP, SERVER_PORT))
