deb592c6c43bbcf439da7301e5de7823878bfeaa
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-07-13 17:26:35 +0100

    Update rcontest.py

--- rcontest.py
+++ rcontest.py
@@ -46,5 +46,14 @@ def main():
     print("Authentication successful!")
 
     # Send "showmap" command
-    send_packet
-
+    send_packet(sock, 2, 2, "help")
+    # Read command response
+    request_id, packet_type, body = receive_packet(sock)
+    # Read the empty response after command
+    receive_packet(sock)
+    print(f"Current map: {body}")
+
+    sock.close()
+
+if __name__ == "__main__":
+    main()
