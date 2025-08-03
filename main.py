2fff4527e0d624a3d56850d72e4ee1fbb0818699
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-08-03 18:01:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ async def main():
         await bot.load_extension("cogs.recon_troop_tracker")
         await bot.load_extension("cogs.certify")
         await bot.load_extension("cogs.LoreCog")
-        await bot.load_extension("cogs.Recruitform")
+        await bot.load_extension("cogs.recruitform")
         # await bot.load_extension("cogs.rcon_tracker")
         await bot.start(TOKEN)
 
