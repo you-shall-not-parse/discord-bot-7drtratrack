e962c923a57742bc0ea0e481d77b3296bc5774c2
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-08-03 18:58:00 +0100

    Update recruitform.py

--- cogs/recruitform.py
+++ cogs/recruitform.py
@@ -6,7 +6,7 @@ FORM_CHANNEL_ID = 1099806153170489485   # Channel where the form embed/button is
 ANSWER_POST_CHANNEL_ID = 1099806153170489485  # Channel where form responses are posted
 
 QUESTIONS = [
-    "Welcome! Please note that if you're on mobile then you may need to click 'commands' to expose the text input for your phone keyboard to pop up!\n What is your current T17 username?",
+    "Welcome! Please note that if you're on mobile then you may need to click 'commands' to expose the text input for your phone keyboard to pop up!\n\n What is your current T17 username?",
     "What is your Age?",
     "What is your country of residence?",
     "What is your timezone?",
@@ -36,8 +36,8 @@ class RecruitFormCog(commands.Cog):
             print(f"Channel ID {FORM_CHANNEL_ID} not found.")
             return
         embed = discord.Embed(
-            title="Recruitment Form",
-            description="We need this info to get you all set up with a platoon. In completing this form I agree to be an active member of this unit, positively contributing to the discord server chats, taking part in training sessions 1-2 times per week and regularly attending Friday events. \n I understand if I don't positively contribute and stop communicating with my platoon, I will be removed from the unit. \n **Click the button below to start your application.**",
+            title="7DR Recruitment Form",
+            description="We need this info to get you all set up with a platoon. In completing this form I agree to be an active member of this unit, positively contributing to the discord server chats, taking part in training sessions 1-2 times per week and regularly attending Friday events. \n\n I understand if I don't positively contribute and stop communicating with my platoon, I will be removed from the unit. \n\n **Click the button below to start your application.**",
             color=discord.Color.blue()
         )
         view = RecruitButtonView(self)
