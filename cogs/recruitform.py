4672f108de844c1ec7fdc9195f81cd5177a112c0
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-08-18 20:08:54 +0100

    Update recruitform.py

--- cogs/recruitform.py
+++ cogs/recruitform.py
@@ -140,8 +140,8 @@ class RecruitFormCog(commands.Cog):
                 "- If you're on mobile, you may need to close the command panel to see the chat by clicking" 
                 " the speech button to the right in order to open the text input to this DM.\n\n"
                 "- You can type 'cancel' at any time to abort and you can restart by clicking the 'start application' button in" 
-                " in #recruitform_requests channel. This form will time-out after 5 minutes. \n\n"
-                "- By completing this form, you agree to follow the #rules, be apositive member of the unit and attend our events 1-2 times per week .\n\n"
+                " in <#1401634001248190515> channel. This form will time-out after 5 minutes. \n\n"
+                "- By completing this form, you agree to follow the rules, be a positive member of the unit and attend our events 1-2 times per week .\n\n"
                 "Please answer the following questions one by one:\n\n"
             )
 
@@ -180,15 +180,15 @@ class RecruitFormCog(commands.Cog):
             # Always post a NEW message; do not update prior ones
             await self.post_answers(user, answers)
             await dm.send(
-            "Thank you! Your answers are now in the #recruitform-responses channel! ✅\n\n"
+            "Thank you! Your answers are now in the <#1098331019364552845> channel! ✅\n\n"
             "2️⃣ If you have not done so already, your next step of the induction process is to"
-            " change your T17 in-game name on Hell Let Loose and post it in <#1098665953706909848> channel.** \n\n" 
+            " change your T17 in-game name on Hell Let Loose and post it in <#1098665953706909848> channel. \n\n" 
             "Your new name must include 'Pte' at the start with the # numbers that show in-game"
             " after you've changed your name, e.g. Pte Mike#6869. If you're struggling check out the induction"
             " video or ask one of our officers! \n\n"
             "3️⃣ Then add your 7DR clan tags on the in-game options menu and you're all set! 🥳 \n\n"
             "🙋‍♂️We have a [video](https://discord.com/channels/1097913605082579024/1365651347415896125/1368867993118834779) which can guide you through all of the above.\n\n"
-            "Discord can be daunting... we have some [tutorial videos](https://discord.com/channels/1097913605082579024/1388800592549511269) to help!/n/n"
+            "Discord can be daunting... we have some [tutorial videos](https://discord.com/channels/1097913605082579024/1388800592549511269) to help! /n/n"
             "😲 We also have <#1099248200776421406> channel for you to add your own discord roles for in game rank, etc"
             )
 
