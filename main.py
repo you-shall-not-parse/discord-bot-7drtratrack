dd383b557524328f6a5f8e5f402e04a706c9005f
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-09-27 19:58:06 +0100

    Add BirthdayCog extension to bot

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ async def main():
         await bot.load_extension("cogs.recruitform")
         await bot.load_extension("cogs.SquadUp")
         await bot.load_extension("cogs.CalendarCog")
+        await bot.load_extension("cogs.BirthdayCog")
         await bot.load_extension("cogs.HLLInfLeaderboard")
         await bot.load_extension("cogs.HLLRecLeaderboard")
         await bot.load_extension("cogs.HLLArmLeaderboard")
