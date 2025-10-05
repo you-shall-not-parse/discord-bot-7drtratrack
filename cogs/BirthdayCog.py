1475dc9dc7717a106671165c9b79dca480bf54ab
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-10-05 12:53:28 +0100

    Update BirthdayCog.py

--- cogs/BirthdayCog.py
+++ cogs/BirthdayCog.py
@@ -6,7 +6,7 @@ from datetime import datetime, time
 import pytz
 
 # ---------------- Config ----------------
-BIRTHDAY_CHANNEL_ID = 1099248200776421406   # channel for birthday embed & daily messages
+BIRTHDAY_CHANNEL_ID = 1098333222540152944   # channel for birthday embed & daily messages
 SUMMARY_CHANNEL_ID = 1098333222540152944    # channel for monthly summaries
 GUILD_ID = 1097913605082579024              # your testing server ID
 TIMEZONE = "Europe/London"
