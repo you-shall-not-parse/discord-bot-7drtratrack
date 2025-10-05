2900a99930335822238b7e605a8d6b8cfe50225a
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-10-05 15:08:00 +0100

    Comment out HLLRecLeaderboard cog loading
    Comment out the loading of HLLRecLeaderboard cog.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def main():
         await bot.load_extension("cogs.BirthdayCog")
         await bot.load_extension("cogs.HLLStatsCog")
         await bot.load_extension("cogs.HLLInfLeaderboard")
-        await bot.load_extension("cogs.HLLRecLeaderboard")
+        # await bot.load_extension("cogs.HLLRecLeaderboard")
         await bot.load_extension("cogs.HLLArmLeaderboard")
         await bot.load_extension("cogs.GameMonCog")
         await bot.load_extension("cogs.GetBackDemon")
