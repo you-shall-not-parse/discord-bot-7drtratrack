1bfa01c4f9b09cfd2107c2be04b83b167d7736ca
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-10-05 15:08:56 +0100

    Update leaderboard title for infantry and recon

--- cogs/HLLInfLeaderboard.py
+++ cogs/HLLInfLeaderboard.py
@@ -118,7 +118,7 @@ class HLLInfLeaderboard(commands.Cog):
 
     async def build_leaderboard_embed(self, monthly: bool = False):
         embed = discord.Embed(
-            title="Hell Let Loose Infantry Leaderboard" + (" - This Month" if monthly else ""),
+            title="Hell Let Loose Infantry & Recon Leaderboard" + (" - This Month" if monthly else ""),
             color=discord.Color.dark_gold(),
         )
         # Add descriptive text under the title
