69e60db20775cb7f8255080509e93f5e207d4186
you-shall-not-parse <103774524+you-shall-not-parse@users.noreply.github.com>
2025-11-23 16:20:46 +0000

    Change setup function to async

--- cogs/mapvote.py
+++ cogs/mapvote.py
@@ -133,5 +133,5 @@ class MapVote(commands.Cog):
 
 
 # --------------------------------------------------
-def setup(bot):
-    bot.add_cog(MapVote(bot))
+async def setup(bot):
+    await bot.add_cog(MapVote(bot))
