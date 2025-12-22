bc8ab452a9331cef2de3ee49f5e86a12cd610e02
benpc <benjamin.app@live.co.uk>
2025-12-22 01:10:20 +0000

    content feed

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ async def main():
         await bot.load_extension("cogs.SquadUp")
         await bot.load_extension("cogs.CalendarCog")
         await bot.load_extension("cogs.BirthdayCog")
+        await bot.load_extension("cogs.contentfeed")
         # await bot.load_extension("cogs.Killfeedcog")
         await bot.load_extension("cogs.mapvote")
         await bot.load_extension("cogs.HLLInfLeaderboard")
